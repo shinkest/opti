df61fd9956a6f2a8ac3fabee28c727ffd5de80e8
arinabilan <arina.bilan@usach.cl>
2025-01-14 23:02:49 -0300

    cambio ejemplos

--- codigo_final_lab2.py
+++ codigo_final_lab2.py
@@ -60,66 +60,66 @@ actividades = ['Educacion_Ambiental', 'Fomento_Reciclaje', 'Economia_Circular']
 
 # Ejemplo 1
 R1 = {'M1': 330900, 'M2': 258300, 'M3': 202600}
-F1 = {'M1': 1000000, 'M2': 800000, 'M3': 600000}
+F1 = {'M1': 9005000, 'M2': 9508000, 'M3': 7770000}
 I1 = {'Educacion_Ambiental': 5000, 'Fomento_Reciclaje': 10000, 'Economia_Circular': 15000}
-C1 = {'Educacion_Ambiental': 100000, 'Fomento_Reciclaje': 200000, 'Economia_Circular': 300000}
+C1 = {'Educacion_Ambiental': 3500000, 'Fomento_Reciclaje': 1230000, 'Economia_Circular': 3507800}
 
 # Ejemplo 2
 R2 = {'M1': 350000, 'M2': 270000, 'M3': 190000}
-F2 = {'M1': 1200000, 'M2': 750000, 'M3': 500000}
+F2 = {'M1': 8008000, 'M2': 9778000, 'M3': 7680900}
 I2 = {'Educacion_Ambiental': 6000, 'Fomento_Reciclaje': 12000, 'Economia_Circular': 18000}
-C2 = {'Educacion_Ambiental': 110000, 'Fomento_Reciclaje': 220000, 'Economia_Circular': 330000}
+C2 = {'Educacion_Ambiental': 3234000, 'Fomento_Reciclaje': 3420900, 'Economia_Circular': 3301000}
 
 # Ejemplo 3
 R3 = {'M1': 400000, 'M2': 250000, 'M3': 220000}
-F3 = {'M1': 1000000, 'M2': 800000, 'M3': 700000}
+F3 = {'M1': 10900000, 'M2': 8700000, 'M3': 7500000}
 I3 = {'Educacion_Ambiental': 7000, 'Fomento_Reciclaje': 15000, 'Economia_Circular': 20000}
-C3 = {'Educacion_Ambiental': 120000, 'Fomento_Reciclaje': 240000, 'Economia_Circular': 360000}
+C3 = {'Educacion_Ambiental': 1200000, 'Fomento_Reciclaje': 2400000, 'Economia_Circular': 3600000}
 
 # Ejemplo 4
 R4 = {'M1': 370000, 'M2': 290000, 'M3': 390000}
-F4 = {'M1': 1300000, 'M2': 850000, 'M3': 690000}
+F4 = {'M1': 13005000, 'M2': 8506000, 'M3': 6990000}
 I4 = {'Educacion_Ambiental': 9000, 'Fomento_Reciclaje': 13000, 'Economia_Circular': 16000}
-C4 = {'Educacion_Ambiental': 100000, 'Fomento_Reciclaje': 250000, 'Economia_Circular': 350000}
+C4 = {'Educacion_Ambiental': 1000000, 'Fomento_Reciclaje': 2500000, 'Economia_Circular': 3500000}
 
 # Ejemplo 5
 R5 = {'M1': 310000, 'M2': 260000, 'M3': 210000}
-F5 = {'M1': 900000, 'M2': 700000, 'M3': 600000}
+F5 = {'M1': 9000000, 'M2': 8070000, 'M3': 6000000}
 I5 = {'Educacion_Ambiental': 8000, 'Fomento_Reciclaje': 14000, 'Economia_Circular': 17000}
-C5 = {'Educacion_Ambiental': 90000, 'Fomento_Reciclaje': 230000, 'Economia_Circular': 320000}
+C5 = {'Educacion_Ambiental': 3000000, 'Fomento_Reciclaje': 1300000, 'Economia_Circular': 2200000}
 
 # Ejemplo 6
-R6 = {'M1': 300000, 'M2': 240000, 'M3': 200000}
-F6 = {'M1': 800000, 'M2': 600000, 'M3': 500000}
-I6 = {'Educacion_Ambiental': 4000, 'Fomento_Reciclaje': 11000, 'Economia_Circular': 15000}
-C6 = {'Educacion_Ambiental': 100000, 'Fomento_Reciclaje': 200000, 'Economia_Circular': 300000}
+R6 = {'M1': 310000, 'M2': 260000, 'M3': 210000}
+F6 = {'M1': 9075000, 'M2': 8930000, 'M3': 6790000}
+I6 = {'Educacion_Ambiental': 8000, 'Fomento_Reciclaje': 14000, 'Economia_Circular': 16000}
+C6 = {'Educacion_Ambiental': 3075000, 'Fomento_Reciclaje': 1304000, 'Economia_Circular': 3650000}
 
 # Ejemplo 7
 R7 = {'M1': 290000, 'M2': 250000, 'M3': 220000}
-F7 = {'M1': 1100000, 'M2': 700000, 'M3': 400000}
+F7 = {'M1': 11060000, 'M2': 7070000, 'M3': 4030000}
 I7 = {'Educacion_Ambiental': 5500, 'Fomento_Reciclaje': 12500, 'Economia_Circular': 16000}
-C7 = {'Educacion_Ambiental': 120000, 'Fomento_Reciclaje': 240000, 'Economia_Circular': 340000}
+C7 = {'Educacion_Ambiental': 1260000, 'Fomento_Reciclaje': 2400060, 'Economia_Circular': 2470000}
 
 # Ejemplo 8
 R8 = {'M1': 350000, 'M2': 270000, 'M3': 200000}
-F8 = {'M1': 950000, 'M2': 650000, 'M3': 550000}
+F8 = {'M1': 57800000, 'M2': 65000000, 'M3': 55000000}
 I8 = {'Educacion_Ambiental': 7000, 'Fomento_Reciclaje': 13000, 'Economia_Circular': 17000}
-C8 = {'Educacion_Ambiental': 110000, 'Fomento_Reciclaje': 220000, 'Economia_Circular': 320000}
+C8 = {'Educacion_Ambiental': 11005000, 'Fomento_Reciclaje': 22000500, 'Economia_Circular': 32007000}
 
 # Ejemplo 9
 R9 = {'M1': 280000, 'M2': 260000, 'M3': 230000}
-F9 = {'M1': 870000, 'M2': 720000, 'M3': 670000}
+F9 = {'M1': 87400000, 'M2': 72020000, 'M3': 97400000}
 I9 = {'Educacion_Ambiental': 4500, 'Fomento_Reciclaje': 12500, 'Economia_Circular': 15500}
-C9 = {'Educacion_Ambiental': 95000, 'Fomento_Reciclaje': 210000, 'Economia_Circular': 310000}
+C9 = {'Educacion_Ambiental': 8577000, 'Fomento_Reciclaje': 21065000, 'Economia_Circular': 21055000}
 
 # Ejemplo 10
 R10 = {'M1': 320000, 'M2': 250000, 'M3': 190000}
-F10 = {'M1': 920000, 'M2': 670000, 'M3': 460000}
+F10 = {'M1': 55205000, 'M2': 67708000, 'M3': 46000000}
 I10 = {'Educacion_Ambiental': 6000, 'Fomento_Reciclaje': 14000, 'Economia_Circular': 19000}
-C10 = {'Educacion_Ambiental': 105000, 'Fomento_Reciclaje': 230000, 'Economia_Circular': 330000}
+C10 = {'Educacion_Ambiental': 10050000, 'Fomento_Reciclaje': 23000000, 'Economia_Circular': 33000000}
 
 # Llamar la función
-resultados, objetivo = optimizar_gestion_residuos(municipalidades, actividades, R1, F1, I1, C1)
+#resultados, objetivo = optimizar_gestion_residuos(municipalidades, actividades, R1, F1, I1, C1)
 #resultados, objetivo = optimizar_gestion_residuos(municipalidades, actividades, R2, F2, I2, C2)
 #resultados, objetivo = optimizar_gestion_residuos(municipalidades, actividades, R3, F3, I3, C3)
 #resultados, objetivo = optimizar_gestion_residuos(municipalidades, actividades, R4, F4, I4, C4)
@@ -128,7 +128,7 @@ resultados, objetivo = optimizar_gestion_residuos(municipalidades, actividades, 
 #resultados, objetivo = optimizar_gestion_residuos(municipalidades, actividades, R7, F7, I7, C7)
 #resultados, objetivo = optimizar_gestion_residuos(municipalidades, actividades, R8, F8, I8, C8)
 #resultados, objetivo = optimizar_gestion_residuos(municipalidades, actividades, R9, F9, I9, C9)
-#resultados, objetivo = optimizar_gestion_residuos(municipalidades, actividades, R10, F10, I10, C10)
+resultados, objetivo = optimizar_gestion_residuos(municipalidades, actividades, R10, F10, I10, C10)
 
 
 # Imprimir resultados
